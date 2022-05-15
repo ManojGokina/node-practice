b2ae38ce0005c77cf6b8e7e706201cac0e8afdc3
ManojGokina <manojgokina@gmail.com>
2022-05-15 17:25:26 +0530

    creating and reading files.

--- third.js
+++ third.js
@@ -3,8 +3,18 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'files');
 console.log(dirPath)
 
-//Creating file in loop
+// Creating file in loop
 
 for(i=0;i<5;i++){
     fs.writeFileSync(dirPath+`/newFile${i}.txt`, 'This is simple text file')
 }
+
+// Reading the files directory
+
+fs.readdir(dirPath , (err , files)=>{
+    console.warn(files)
+
+    files.forEach((item)=>{
+      console.log(item)
+    })
+})
